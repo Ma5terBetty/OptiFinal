aa10067d83d462cc847cdc71757351ab63f95f6d
Nicolás Massara <nicolasmassara@outlook.com>
2023-07-17 21:12:58 -0300

    Gameplay components added

---
+++ Assets/Scripts/Gameplay Elements/BallController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Gameplay_Elements
+{
+    public class BallController : MonoBehaviour
+    {
+        [SerializeField] private Transform startPos;
+        [SerializeField] private Ball ballPrefab;
+
+        private List<Ball> _balls = new List<Ball>();
+        private bool _isAbleToThrow;
+
+        public int BallsInWorld => _balls.Count;
+        
+        private void Start()
+        {
+            AddBall(startPos.position);
+        }
+
+        public void ThrowBall()
+        {
+            if (!_isAbleToThrow) return;
+            
+            _balls[0].Throw();
+            _isAbleToThrow = false;
+        }
+
+        public void AddBall(Vector3 position)
+        {
+            var newBall = Instantiate(ballPrefab, position, Quaternion.identity);
+            newBall.OnDestroyed += Ball_OnDestroyedHandler;
+            _balls.Add(newBall);
+
+            if (BallsInWorld == 1)
+            {
+                Reset();
+            }
+        }
+
+        public void Reset()
+        {
+            var ball = _balls[0];
+            
+            ball.SetThrowPos(startPos);
+            ball.Freeze();
+            _isAbleToThrow = true;
+        }
+
+        private void Ball_OnDestroyedHandler(Ball ball)
+        {
+            if (BallsInWorld > 1)
+            {
+                _balls.Remove(ball);
+                ball.OnDestroyed -= Ball_OnDestroyedHandler;
+                return;
+            }
+            
+            Reset();
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,48 +2,36 @@ using System;
 using Custom.Physics;
 using UnityEngine;
 using Custom.UpdateManager;
+using Unity.VisualScripting;
 using Random = UnityEngine.Random;
 
-public class Player : UpdateBehavior, IGameplayUpdate
+public class Player : MonoBehaviour
 {
     [SerializeField] private float playerSpeed = 5;
 
     private ICollider _collider;
     private IBody _body;
-    private bool _hasCollider;
+    private bool _hasBody;
 
     private void Awake()
     {
         _collider = GetComponent<ICollider>();
         _body = GetComponent<IBody>();
 
-        _hasCollider = _collider != null;
-    }
 
-    private void Start()
-    {
-        _collider.OnCollision += OnCollisionHandler;
-    }
-
-    public void Tick()
-    {
-        PlayerInput();
-    }
-
-    private void PlayerInput()
-    {
-        if (Input.GetKey(KeyCode.A))
+        _hasBody = _body != null;
+        
+        if (_collider != null)
         {
-            _body.SetVelocity(Vector3.right * -playerSpeed);
+            _collider.OnCollision += OnCollisionHandler;
         }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            _body.SetVelocity(Vector3.right * playerSpeed);
-        }
-        else
-        { 
-            _body.Freeze();
-        }
+    }
+
+    public void Move(float input)
+    {
+        if (!_hasBody) return;
+        
+        _body.SetVelocity(Vector3.right * (playerSpeed * input));
     }
 
     private void OnCollisionHandler(ICollider other)
@@ -54,8 +42,8 @@ public class Player : UpdateBehavior, IGameplayUpdate
 
         var distToCenter = Vector3.Distance(transform.position,other.Position);
 
-        int rnd = Random.Range(2, 3);
+        int rnd = Random.Range(1, 2);
         
-        ball.SetDirection(distToCenter < 1 ? 1 : rnd);
+        ball.SetDirection(distToCenter < 1 ? 0 : rnd);
     }
 }
